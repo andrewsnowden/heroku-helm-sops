636fb61c5d803fc8162e6ff121f5d7a8ae03f64b
Andrew Snowden <andrew@yoco.com>
2023-06-27 18:36:53 +0200

    .

--- .profile.d/foo.py
+++ .profile.d/foo.py
@@ -1,7 +1,6 @@
 import os
 
-print(f"cwd: ${os.cwd()}")
-s = os.popen(f"yq eval '.defaults.env' ../values.yaml")
+s = os.popen(f"yq eval '.defaults.env' values.yaml")
 output = s.read()
 
 print("-" * 80)
