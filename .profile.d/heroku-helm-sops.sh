32afc84ffc0177bb0145d357090050ef7c16c603
Andrew Snowden <andrew@yoco.com>
2023-06-27 19:30:54 +0200

    .

--- .profile.d/heroku-helm-sops.sh
+++ .profile.d/heroku-helm-sops.sh
@@ -1,11 +1,5 @@
 export PATH="$PATH:$HOME/.heroku-helm-sops/"
 
-echo "Decrypting secrets using SOPS"
-AWS_SECRET_ACCESS_KEY=$SOPS_AWS_SECRET_ACCESS_KEY AWS_ACCESS_KEY_ID=$SOPS_AWS_ACCESS_KEY_ID sops --decrypt secrets.yaml | yq eval .defaults.secrets -o=json >> decrypted.json
-
-echo "Loading environment variables from values.yaml and secrets.yaml"
-python .profile.d/load-env.py
-
-rm decrypted.json
+python .profile.d/load-from-chart.py
 
 source .profile.d/values
